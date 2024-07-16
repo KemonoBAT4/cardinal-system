9475848dc53c07153bfd368f7266ea2554336dbf
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-07-16 23:46:49 +0200

    created flask api

--- Logging/cardinalLogger.py
+++ Logging/cardinalLogger.py
@@ -3,16 +3,18 @@ import logging
 
 from datetime import datetime
 
+# Log Class, Should be called only by Cardinal itself
 class CardinalLogger():
+    
     logger = logging.getLogger(__name__)
     logging.basicConfig(filename='cardinal.log', encoding='utf-8', level=logging.DEBUG)
 
-    def debug(message):
-        logger.debug(message)
+    def debug(self, message):
+        self.logger.debug(datetime.now() + ": " + message)
 
-    def warning(message):
-        logger.warning(message)
+    def warning(self, message):
+        self.logger.warning(datetime.now() + ": " + message)
 
-    def error(message):
-        logger.error(message)
+    def error(self, message):
+        self.logger.error(datetime.now() + ": " + message)
 
