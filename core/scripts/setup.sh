522beee557b55c9231cc04ab851c6a16fcea1d74
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-09-25 09:58:39 +0200

    [core] modified bash files to run & setup the application

---
+++ core/scripts/setup.sh
@@ -0,0 +1,11 @@
+
+# setup the virtual env
+python3 -m venv venv
+source venv/bin/activate
+
+# install python requirements
+pip install -r requirements.txt
+
+# install node dependecies
+cd apps/Discord_bot
+npm install
