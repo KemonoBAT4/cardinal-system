f97ebbcb6eac4b5bc5ab4ffdea7c706ac7926237
KemonoBAT4 <kemono.bat.4@gmail.com>
2024-12-24 00:28:14 +0100

    [cardinal] minor changes

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from core.Cardinal.cardinal import Cardinal
 def main():
     # initializing cardinal as master
     system = Cardinal()
-    
+#enddef
 
 # start the program
 if __name__ == "__main__":
     main()
+#endif
