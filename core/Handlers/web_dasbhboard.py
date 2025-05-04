a31e211278810af3860a7ff729d86a979f868467
KemonoBAT4 <kemono.bat.4@gmail.com>
2025-05-04 23:04:21 +0200

    [core] created web html for dashboard

---
+++ core/Handlers/web_dasbhboard.py
@@ -0,0 +1,9 @@
+"""
+here goes the api handler of the front-api in 'api/cardinalApi.py'
+in this file will be handled data storing
+"""
+from flask import render_template, send_file
+import os
+
+
+
