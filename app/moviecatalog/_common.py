c8220d8deef565a82db2e72a76f0978bd53669d7
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-06-26 07:28:06 +0200

    [moviecatalog] added small updates (need to remove the moviecatalog project)

--- app/moviecatalog/_common.py
+++ app/moviecatalog/_common.py
@@ -1,9 +1,10 @@
 
 # flask imports
-from flask import Blueprint, redirect, url_for, request                         # type: ignore
-from flask import render_template, send_from_directory                          # type: ignore
-from flask_login import login_required                                          # type: ignore
+from flask import Blueprint, redirect, url_for, request, jsonify                     # type: ignore
+from flask import render_template, send_from_directory                               # type: ignore
+from flask_login import login_required                                               # type: ignore
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from sqlalchemy import and_, or_
 
 # core imports
 from core.models import *
@@ -15,10 +16,11 @@ from core.system import *
 # other imports
 import os
 import typing
+import datetime
 from enum import Enum
 
-
 project_name: str = os.path.dirname(os.path.abspath(__file__))
+
 
 # "D:\documents\projects\moviecatalog"
 MEDIA_FOLDER: str = cardinal.config.get("Cardinal Custom", "MEDIA_FOLDER", fallback="/")
