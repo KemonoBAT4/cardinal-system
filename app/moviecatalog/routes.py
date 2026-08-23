036244c1c581f2c6a4eb0cf5595bab02f775d611
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-08-23 23:53:37 +0200

    [moviecatalog] added page title to the dashboard movie list page

--- app/moviecatalog/routes.py
+++ app/moviecatalog/routes.py
@@ -32,7 +32,7 @@ def home():
 @routes.route("/dashboard/movie/list", methods=['GET'])
 def movie_list():
 
-    page = Page(title="Lista Film")
+    page = Page(page_title = "Movie Catalog", title="Lista Film")
     card = Card("Lista di tutti i Film")
 
     movie_list_section = Section(title = "Lista di tutti i Film Presenti").table(
