bee25bfd2bc77185d6a39b0e7cebb029500e6658
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-08-23 23:59:31 +0200

    [web] added Calendar widget template

---
+++ core/web/widgets/calendar.py
@@ -0,0 +1,9 @@
+
+from ._common import *
+
+# NOTE: this class is not yet implemented
+# TODO: implement this class
+# the class should create a interagible calendar to display events
+class CardinalCalendar(CardinalBaseWidget):
+    ...
+# #endclass CardinalCalendar
