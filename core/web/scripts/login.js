792fccad0c81f98ff5a6c6b3b0dd44140ce56ed5
KemonoBAT4 <kemono.bat.4@gmail.com>
2026-08-25 02:16:41 +0200

    [web] updated login & register scripts, styles and template structure, with new features on the error display and how data is treated

--- core/web/scripts/login.js
+++ core/web/scripts/login.js
@@ -1,11 +1,46 @@
 
+function showToast(message) {
+    const container = document.getElementById('toast-container');
+
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+
+    toast.innerHTML = `
+        <div class="toast-icon">!</div>
+        <div class="toast-message">${message}</div>
+    `;
+
+    container.appendChild(toast);
+
+    setTimeout(() => {
+        toast.remove();
+    }, 5000);
+}
+
 // dashboard base page script
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Web application initialized');
 
-    let info_text = document.querySelector('.infos-text');
+    const form = document.querySelector('.login-form');
 
-    info_text.addEventListener('click', () => {
-        window.location.href = '/about';
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        let response = await fetch("/auth/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({
+                username: document.querySelector('#username').value,
+                password: document.querySelector('#password').value
+            })
+        }).then(response => response.json());
+
+        if (response.status == true) {
+            window.location.href = "/home";
+        } else {
+            showToast(response.message);
+        }
     });
 });
